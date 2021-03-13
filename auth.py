c8579e784fb292a58c3180b7798949d85bd9e2ee
Awad Ahmed <32722982+awadahmed97@users.noreply.github.com>
2021-03-12 21:29:24 -0500

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,169 @@
+import json
+from flask import request, _request_ctx_stack
+from functools import wraps
+from jose import jwt
+from urllib.request import urlopen
+from flask import session
+from werkzeug.exceptions import HTTPException
+from authlib.integrations.flask_client import OAuth
+from six.moves.urllib.parse import urlencode
+import os
+
+#AUTH0_DOMAIN = 'fsndc.us.auth0.com'
+#ALGORITHMS = ['RS256']
+#API_AUDIENCE = 'coffee'
+
+AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
+ALGORITHMS = os.getenv('ALGORITHMS')
+API_AUDIENCE = os.getenv("API_AUDIENCE")
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+AUTH0_CALLBACK_URL = os.getenv('AUTH0_CALLBACK_URL')
+
+def setup_auth0():
+    global AUTH0_DOMAIN
+    global ALGORITHMS
+    global API_AUDIENCE
+    global CLIENT_ID
+    global CLIENT_SECRET
+    global AUTH0_CALLBACK_URL
+    AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
+    ALGORITHMS = os.getenv('ALGORITHMS')
+    API_AUDIENCE = os.getenv("API_AUDIENCE")
+    CLIENT_ID = os.getenv('CLIENT_ID')
+    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+    AUTH0_CALLBACK_URL = os.getenv('AUTH0_CALLBACK_URL')
+
+## AuthError Exception
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+## Auth Header
+
+
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    token = parts[1]
+    return token
+
+
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        print("premissions not in token")
+        raise AuthError({
+                            'code': 'invalid_claims',
+                            'description': 'Permissions not included in JWT.'
+                        }, 400)
+    elif permission not in payload['permissions']:
+        raise AuthError({
+                            'code': 'invalid_claims',
+                            'description': 'Permissions not included in JWT.'
+                        }, 403)
+    else:
+        return True
+
+
+
+def verify_decode_jwt(token):
+    jsonurl = urlopen(f'https://dev-ag123.us.auth0.com/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms= ALGORITHMS,
+                audience= API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            print("entered 7")
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+
+def validate_token_structure(token):
+    parts = token.split('.')
+    if(len(parts) != 3):
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must include valid token structure (*.*.*).'
+        }, 401)
+
+
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            validate_token_structure(token)
+            payload = verify_decode_jwt(token)
+            check_permissions(permission, payload)
+            return f(*args, **kwargs)
+
+        return wrapper
+    return requires_auth_decorator
